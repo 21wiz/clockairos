b45800916888d5f2724ad08e1cf334779e0f9d21
21wiz <jalcantara91@gmail.com>
2016-02-10 02:49:59 -0600

    Added receivable sms

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
-  resources :appointments, :sessions
+  resources :appointments, :sessions, :twilio
   
   # You can have the root of your site routed with "root"
   root 'appointments#welcome'
+  
+  get 'twilio/process_sms' => 'twilio#process_sms'
   #get 'login', to: 'sessions#login', as: :login
   #login_url 'appointments#login'
 end
