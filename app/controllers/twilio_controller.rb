451c448277c4bac4eb60ec044a623ac53a2af5be
21wiz <jalcantara91@gmail.com>
2016-02-10 09:29:36 -0600

    Fixed the call to give the name and area

--- app/controllers/twilio_controller.rb
+++ app/controllers/twilio_controller.rb
@@ -2,7 +2,6 @@ require 'twilio-ruby'
 
 class TwilioController < ApplicationController
   include Webhookable
-  before_action :set_ifcall
 
   after_filter :set_header
 
@@ -12,28 +11,19 @@ class TwilioController < ApplicationController
   def process_sms
     @city = params[:FromCity].capitalize
     @state = params[:FromState]
-    which_state = params[:FromState]
-    id_state= params[:Body]
-    if which_state==id_state
-        render 'process_sms.xml.erb', :content_type => 'text/xml'
-        ifcall = "0"
-    else
-        render 'process_sms2.xml.erb', :content_type => 'text/xml'
-        ifcall = "1"
-    end
+    id_app= params[:Body]
+     #render 'process_sms.xml.erb', :content_type => 'text/xml'
+     render 'process_sms2.xml.erb', :content_type => 'text/xml'
   end
 
 # POST /twilio/voice
   def voice
-      if ifcall=="1"
-        response = Twilio::TwiML::Response.new do |r|
-          r.Say 'Hey there. Congrats on finding out that you an also call Clock Kairos', :voice => 'alice'
-          r.Play 'http://linode.rabasa.com/cantina.mp3'
-        render_twiml response
-        break
-      else
-        break
-      end
+  	response = Twilio::TwiML::Response.new do |r|
+  	  r.Say 'Hey there. Congrats on finding out that you an also call Clock Kairos', :voice => 'alice'
+  	  r.Play 'http://linode.rabasa.com/cantina.mp3'
+  	end
+
+  	render_twiml response
   end
 
 # POST /twilio/call
@@ -42,20 +32,13 @@ class TwilioController < ApplicationController
     citystr = params[:FromCity]
     @state = params[:FromState]
     statestr = params[:FromState]
-    if ifcall=="1"
-      response = Twilio::TwiML::Response.new do |r|
-        r.Say "Hey there . Congrats waking up on time! It is a beautiful day in #{citystr}, #{statestr}. Now gather your energy and GO CHANGE THE WORLD!", :voice => 'man'
-        r.Play 'http://linode.rabasa.com/cantina.mp3'
-      render_twiml response
-    else
-      ifcall="1"
-      break
-    end
+    #namestr = appointment_params[:name]
+  	response = Twilio::TwiML::Response.new do |r|
+  	  r.Say "Hey there . Congrats waking up on time! It is a beautiful day in #{citystr}, #{statestr}. Now gather your energy and GO CHANGE THE WORLD!", :voice => 'man'
+  	  r.Play 'http://linode.rabasa.com/cantina.mp3'
+  	end
+
+  	render_twiml response
   end
 
-  private 
-    def set_ifcall
-      ifcall=1
-    end
-  end
 end
