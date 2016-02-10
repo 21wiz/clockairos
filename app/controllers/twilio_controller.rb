e1812bf25450a5f7d7d97c6bf3a4815a6e04ed2b
21wiz <jalcantara91@gmail.com>
2016-02-10 06:55:26 -0600

    Fixed the call to give the name and area

--- app/controllers/twilio_controller.rb
+++ app/controllers/twilio_controller.rb
@@ -2,7 +2,6 @@ require 'twilio-ruby'
 
 class TwilioController < ApplicationController
   include Webhookable
-  before_action :set_appointment, only: [:show, :edit, :update, :destroy]
 
   after_filter :set_header
 
@@ -12,10 +11,11 @@ class TwilioController < ApplicationController
   def process_sms
     @city = params[:FromCity].capitalize
     @state = params[:FromState]
-    yourPhone = "+1"+params[Body]
+    yourPhone = "+1"+params[:Body]
+    appthere = Appointment.first(:conditions => "phone_number = params[:Body]")
     if yourPhone.include? params[:from]
         render 'process_sms.xml.erb', :content_type => 'text/xml'
-        @appointment.destroy
+        appthere.destroy
       else
         render 'process_sms2.xml.erb', :content_type => 'text/xml'
       end
@@ -37,25 +37,13 @@ class TwilioController < ApplicationController
     citystr = params[:FromCity]
     @state = params[:FromState]
     statestr = params[:FromState]
-    namestr = appointment_params[:name]
+    #namestr = appointment_params[:name]
   	response = Twilio::TwiML::Response.new do |r|
-  	  r.Say 'Hey there #{namestr}. Congrats waking up on time! It is a beautiful day in #{citystr}, #{statestr}. Now gather your energy and GO CHANGE THE WORLD!', :voice => 'alice'
+  	  r.Say 'Hey there . Congrats waking up on time! It is a beautiful day in #{citystr}, #{statestr}. Now gather your energy and GO CHANGE THE WORLD!', :voice => 'alice'
   	  r.Play 'http://linode.rabasa.com/cantina.mp3'
   	end
 
   	render_twiml response
   end
-
-  private
-    # Use callbacks to share common setup or constraints between actions.
-    # See above ---> before_action :set_appointment, only: [:show, :edit, :update, :destroy]
-    def set_appointment
-      @appointment = Appointment.find(params[:id])
-    end
-
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def appointment_params
-      params.require(:appointment).permit(:name, :phone_number, :time, :time_zone)
-    end
 
 end
